1bd113926d21658ce3379f7e4bf8d65b25f6f4c3
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-04-13 11:17:03 -0600

    Add three tests

--- tests/server-test.js
+++ tests/server-test.js
@@ -12,6 +12,25 @@ const database = require('knex')(configuration);
 chai.use(chaiHttp);
 
 describe('Server', () => {
+  beforeEach(function(done) {
+    knex.migrate.rollback()
+    .then(function() {
+      knex.migrate.latest()
+      .then(function() {
+        return database.seed.run()
+        .then(function() {
+          done();
+        });
+      });
+    });
+  });
+  afterEach((done)=>{
+    knex.migrate.rollback()
+    .then(()=>{
+      done()
+    })
+  });
+
   it('should exist', () => {
     expect(app).to.exist
   });
@@ -20,14 +39,42 @@ describe('Server', () => {
     it('should return html successfully',(done)=>{
       chai.request(app)
       .get('/')
-      .end((err,res)=>{
-        if(err) { done(err); }
-        expect(res).to.have.status(200)
-        expect(res).to.be.html
+      .end((error, response)=>{
+        if(error) { done(error); }
+        expect(response).to.have.status(200)
+        expect(response).to.be.html
         done();
       });
     });
   });
 
+//get all the garage items
+  describe('GET /api/v1/junk', () => {
+    it('should retrieve the junk', (done) => {
+      chai.request(app)
+      .get('/api/v1/junk')
+      .end((error, response) => {
+        if(error) { done(error); }
+        expect(response).to.have.status(200)
+        expect(response).to.be.json
+        expect(response).to.be.a('array')
+        expect(response).to.have.length(3)
+        done();
+      });
+    });
+  });
 
+//get one item from the garage
+  describe('GET /api/v1/junk/:id', () => {
+    it('should retrieve one item from the garage', (done) => {
+      chai.request(app)
+      .get('/api/v1/junk/1')
+      .end((error, response) => {
+        if(error) { done(error); }
+        expect(response).to.have.status(200)
+        expect(response).to.be.json
+        done();
+      })
+    })
+  })
 });
